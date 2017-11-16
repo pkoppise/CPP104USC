e91645d21d5cfd8591eeab78811851dce18da7bd
Your Name <you@example.com>
2017-11-16 09:13:06 +0530

    String

---
+++ basicstring.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+	string x = "hello";
+	x += " world!";
+	cout<< x << endl;
+	cout<< "The string is " << x.size() << " chars long." << endl;
+	cout<< "The string is " << x.length() << " chars long." << endl;
+	cout<< "The first character is " << x.at(0) << endl;
+
+	string y = x.substr(0,4);
+	string z = x.substr(1,4);
+    cout << y << endl;
+    cout << z << endl;
+    cout<< "The letter 'l' is at index " << y.find('l') << endl;
+    return 0;
+}
